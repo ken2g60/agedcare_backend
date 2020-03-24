9c98c0146e07e44fe4d4a56d541bae464dc2ec74
knight <knightshell301@gmail.com>
2020-03-24 11:49:12 +0000

    models created for pressure, weight and glucose

--- health/admin.py
+++ health/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 
-from .models import HealthData
+from .models import HealthData, Glucose, Pressure, Weight
 
-# Register your models here.
+
 class HealthAdmin(admin.ModelAdmin):
     list_display = ('userId', 'phonenumber', 'bloodsugar', 'bloodpressure', 'bloodcholesterol', 'bloodlevel', 'weight', 'created_at')
 
+class GlucoseAdmin(admin.ModelAdmin):
+    list_display = ('userId', 'glucose', 'created_at')
+
+class PressureAdmin(admin.ModelAdmin):
+    list_display = ('userId', 'systolic', 'diastolic', 'created_at')
+
+
+class WeightAdmin(admin.ModelAdmin):
+    list_display = ('userId', 'weight')
+
+    
+    
 admin.site.register(HealthData, HealthAdmin)
+admin.site.register(Glucose, GlucoseAdmin)
+admin.site.register(Pressure, PressureAdmin)
+admin.site.register(Weight, WeightAdmin)
