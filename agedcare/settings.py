cb91a5dd1ddd1f86ba660ee9cb8cd2bf96a7c897
knight <knightshell301@gmail.com>
2020-04-22 11:59:15 +0000

    post/models.py updated

--- agedcare/settings.py
+++ agedcare/settings.py
@@ -81,6 +81,7 @@ WSGI_APPLICATION = 'agedcare.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
--- post/models.py
+++ post/models.py
@@ -5,5 +5,4 @@ class Post(models.Model):
     title = models.CharField(_("Blog Title"), max_length=50)
     image = models.FileField(_("Blog Image"), upload_to='images/')
     description = models.TextField(blank=True)
-    posted_time = models.DateField(_("Posted Date"), auto_now_add=False)
     created_at = models.DateTimeField(auto_now_add=True)
