5aade2ddb931538351a096f6434fb3693bcfc7fa
knight <knightshell301@gmail.com>
2020-05-07 09:12:32 +0000

    agedcare/settings.py updated

--- agedcare/settings.py
+++ agedcare/settings.py
@@ -169,15 +169,7 @@ STATICFILES_DIRS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.UserRateThrottle'
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    ]
 }
 
 MEDIA_URL = '/media/'
