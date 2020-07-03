3a698016e23bbdd4dcc6fa2b10fdb8b7739cce64
knight <knightshell301@gmail.com>
2020-07-03 00:40:08 +0000

    fixing token for android

--- agedcare/settings.py
+++ agedcare/settings.py
@@ -179,12 +179,12 @@ STATIC_URL = '/static/'
 #     ],
 # }
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.BasicAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#     )
+# }
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
